1ef200924014a8a0f85eec02d11c2a7f98c67ba3
Cypher45 <anvirsingh51@gmail.com>
2026-03-25 14:36:54 +0530

    Contact js

---
+++ Contact-us/contact.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".contact-form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+    const firstName = form.querySelector('input[placeholder="FIRST NAME"]').value;
+    const lastName = form.querySelector('input[placeholder="LAST NAME"]').value;
+    const email = form.querySelector('input[type="email"]').value;
+    const phone = form.querySelector('input[type="tel"]').value;
+
+    const contactData = {
+        firstName,
+        lastName,
+        email,
+        phone
+    };
+    let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+    contacts.push(contactData);
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+    alert("Your message has been saved successfully!");
+
+    form.reset();
+});
